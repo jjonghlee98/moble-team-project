f384ff37fa75ca5824b9b13e046f4b96e10d980d
jhwanl <jjonghlee98@gmail.com>
2022-10-07 15:48:00 +0900

    jeogi project radius step

--- templates/js/findCourseMap.js
+++ templates/js/findCourseMap.js
@@ -3,7 +3,7 @@ const locationButton = document.getElementById("location_button");
 var mapContainer = document.getElementById("map"), // 지도를 표시할 div
   mapOption = {
     center: new kakao.maps.LatLng(36.815063, 127.1580072), // 지도의 중심좌표
-    level: 6, // 지도의 확대 레벨
+    level: 4, // 지도의 확대 레벨
   };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
@@ -23,6 +23,30 @@ function panTo() {
   // 지도 중심을 부드럽게 이동시킵니다
   // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
   map.panTo(moveLatLon);
+}
+
+// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+function displayMarker(locPosition, message) {
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    map: map,
+    position: locPosition,
+  });
+
+  var iwContent = message, // 인포윈도우에 표시할 내용
+    iwRemoveable = true;
+
+  // 인포윈도우를 생성합니다
+  var infowindow = new kakao.maps.InfoWindow({
+    content: iwContent,
+    removable: iwRemoveable,
+  });
+
+  // 인포윈도우를 마커위에 표시합니다
+  infowindow.open(map, marker);
+
+  // 지도 중심좌표를 접속위치로 변경합니다
+  map.setCenter(locPosition);
 }
 
 // 내위치 버튼
