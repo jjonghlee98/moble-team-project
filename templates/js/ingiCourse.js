d2897552f16da829f18b6a62f9a48720ef0ce0c8
jhwanl <jjonghlee98@gmail.com>
2022-10-13 18:02:58 +0900

    ingiCourse

---
+++ templates/js/ingiCourse.js
@@ -0,0 +1,219 @@
+const data = [
+  {
+    documents: [
+      {
+        place_url: "http://place.map.kakao.com/287484837",
+        place_name: "그레이츄",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 오성7길 42",
+        category_name: "음식점 > 카페 > 테마카페 > 디저트카페",
+        distance: "386",
+        phone: "",
+        category_group_code: "CE7",
+        x: "127.13256939448158",
+        y: "36.832239894673286",
+        address_name: "충남 천안시 서북구 두정동 749",
+        id: "287484837",
+      },
+      {
+        place_url: "http://place.map.kakao.com/17884789",
+        place_name: "스타벅스 천안두정점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 부성4길 3",
+        category_name: "음식점 > 카페 > 커피전문점 > 스타벅스",
+        distance: "420",
+        phone: "",
+        category_group_code: "CE7",
+        x: "127.134840222862",
+        y: "36.83398729098",
+        address_name: "충남 천안시 서북구 두정동 975-1",
+        id: "17884789",
+      },
+      {
+        place_url: "http://place.map.kakao.com/13064309",
+        place_name: "투썸플레이스 천안두정점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 부성1길 3",
+        category_name: "음식점 > 카페 > 커피전문점 > 투썸플레이스",
+        distance: "489",
+        phone: "041-522-8899",
+        category_group_code: "CE7",
+        x: "127.13565406687803",
+        y: "36.833980057467116",
+        address_name: "충남 천안시 서북구 두정동 973",
+        id: "13064309",
+      },
+      {
+        place_url: "http://place.map.kakao.com/1396273892",
+        place_name: "헬로밀가루 천안점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 두정로 127",
+        category_name: "음식점 > 카페 > 테마카페 > 키즈카페",
+        distance: "138",
+        phone: "041-522-2535",
+        category_group_code: "CE7",
+        x: "127.130993103758",
+        y: "36.8341069334416",
+        address_name: "충남 천안시 서북구 두정동 1989",
+        id: "1396273892",
+      },
+      {
+        place_url: "http://place.map.kakao.com/209092160",
+        place_name: "977번지",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 부성4길 10",
+        category_name: "음식점 > 카페 > 테마카페",
+        distance: "432",
+        phone: "0507-1343-0699",
+        category_group_code: "CE7",
+        x: "127.135173019274",
+        y: "36.834541994932",
+        address_name: "충남 천안시 서북구 두정동 977",
+        id: "209092160",
+      },
+      {
+        place_url: "http://place.map.kakao.com/19336929",
+        place_name: "할리스 천안두정점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 오성6길 52",
+        category_name: "음식점 > 카페 > 커피전문점 > 할리스",
+        distance: "469",
+        phone: "041-574-0013",
+        category_group_code: "CE7",
+        x: "127.1331423030561",
+        y: "36.83164091929133",
+        address_name: "충남 천안시 서북구 두정동 779",
+        id: "19336929",
+      },
+      {
+        place_url: "http://place.map.kakao.com/1321417319",
+        place_name: "읍천리382 천안두정점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 오성8길 39",
+        category_name: "음식점 > 카페",
+        distance: "312",
+        phone: "041-414-0382",
+        category_group_code: "CE7",
+        x: "127.132372156582",
+        y: "36.832923153661",
+        address_name: "충남 천안시 서북구 두정동 716",
+        id: "1321417319",
+      },
+      {
+        place_url: "http://place.map.kakao.com/18856046",
+        place_name: "탐앤탐스 천안에비뉴점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 서부대로 739",
+        category_name: "음식점 > 카페 > 커피전문점 > 탐앤탐스",
+        distance: "336",
+        phone: "041-569-3110",
+        category_group_code: "CE7",
+        x: "127.13315842541924",
+        y: "36.833181794528194",
+        address_name: "충남 천안시 서북구 두정동 701",
+        id: "18856046",
+      },
+      {
+        place_url: "http://place.map.kakao.com/27373262",
+        place_name: "빽다방 천안두정중앙점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 두정로 166",
+        category_name: "음식점 > 카페 > 커피전문점 > 빽다방",
+        distance: "471",
+        phone: "041-562-5552",
+        category_group_code: "CE7",
+        x: "127.1352486516287",
+        y: "36.83356601013033",
+        address_name: "충남 천안시 서북구 두정동 910",
+        id: "27373262",
+      },
+      {
+        place_url: "http://place.map.kakao.com/547608158",
+        place_name: "이디야커피 천안두정점",
+        category_group_name: "카페",
+        road_address_name: "충남 천안시 서북구 부성6길 8",
+        category_name: "음식점 > 카페 > 커피전문점 > 이디야커피",
+        distance: "362",
+        phone: "041-569-9777",
+        category_group_code: "CE7",
+        x: "127.134435063096",
+        y: "36.8356385774568",
+        address_name: "충남 천안시 서북구 두정동 991",
+        id: "547608158",
+      },
+    ],
+    meta: {
+      total_count: 46,
+      is_end: false,
+      pageable_count: 45,
+      same_name: null,
+    },
+  },
+];
+
+const h1 = document.getElementsByTagName("h1");
+const cafeRoadAddress = document.getElementsByClassName('road_address');
+const cafeAddress = document.getElementsByClassName('address');
+const phoneNum = document.getElementsByClassName('phone');
+
+const cafeInfo = data[0]["documents"];
+
+for (let i = 0; i < cafeInfo.length; i++) {
+  h1[i].innerText = cafeInfo[i]['place_name'];
+  cafeRoadAddress[i].innerText = cafeInfo[i]['road_address_name'];
+  cafeAddress[i].innerText = cafeInfo[i]['address_name'];
+  if (cafeInfo[i]['phone'] != '') {
+    console.log(cafeInfo[i]['phone']);
+    phoneNum[i].innerText = cafeInfo[i]['phone'];
+  }
+
+  let locPosition = new kakao.maps.LatLng(cafeInfo[i]["y"], cafeInfo[i]["x"]),
+    message = cafeInfo[i]["place_name"];
+
+  console.log(locPosition, message);
+  displayMarker(locPosition, message);
+}
+
+// 반경 설정 여기선 문제가 많습니다..
+const radiusButton = document.querySelectorAll("button");
+var circle = new kakao.maps.Circle();
+
+radiusButton.forEach((event) => {
+  event.addEventListener("click", () => {
+    radiusButton.forEach((e) => {
+      e.classList.remove("active-color");
+      circle.setMap(null);
+    });
+    event.classList.add("active-color");
+    console.log(event.value);
+    radius = Number(event.value);
+
+    let x = 0;
+    let y = 0;
+
+    for (let i = 0; i < cafeInfo.length; i++) {
+      h1[i].addEventListener("click", () => {
+        console.log("x: " + cafeInfo[i]["x"] + "\n" + "y: " + cafeInfo[i]["y"]);
+      });
+      x = cafeInfo[i]["x"];
+      y = cafeInfo[i]["y"];
+    }
+
+    // 지도에 표시할 원을 생성합니다
+    circle = new kakao.maps.Circle({
+      center: new kakao.maps.LatLng(y, x), // 원의 중심좌표 입니다
+      radius: radius, // 미터 단위의 원의 반지름입니다
+      strokeWeight: 2, // 선의 두께입니다
+      strokeColor: "#FF5757", // 선의 색깔입니다
+      strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      strokeStyle: "solid", // 선의 스타일 입니다
+      // fillColor: "#CFE7FF", // 채우기 색깔입니다
+      fillColor: "#FF5757", // 채우기 색깔입니다
+      fillOpacity: 0.7, // 채우기 불투명도 입니다
+    });
+
+    // 지도에 원을 표시합니다
+    circle.setMap(map);
+  });
+});
+// 반경 설정
---
+++ templates/js/ingiMap.js
@@ -0,0 +1,67 @@
+const locationButton = document.getElementById("location_button");
+
+var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+  mapOption = {
+    center: new kakao.maps.LatLng(36.815063, 127.1580072), // 지도의 중심좌표
+    level: 4, // 지도의 확대 레벨
+  };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커 커스텀 인데여;;
+var imageSrc = "../images/marker_img.png",
+  imageSize = new kakao.maps.Size(64, 69),
+  imageOption = { offset: new kakao.maps.Point(27, 69) };
+
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+// 마커 커스텀 인데여;;
+
+function setCenter() {
+  // 이동할 위도 경도 위치를 생성합니다
+  var moveLatLon = new kakao.maps.LatLng(33.452613, 126.570888);
+
+  // 지도 중심을 이동 시킵니다
+  map.setCenter(moveLatLon);
+}
+
+function panTo() {
+  // 이동할 위도 경도 위치를 생성합니다
+  var moveLatLon = new kakao.maps.LatLng(33.45058, 126.574942);
+
+  // 지도 중심을 부드럽게 이동시킵니다
+  // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+  map.panTo(moveLatLon);
+}
+
+// 지도에 마커와 인포윈도우를 표시하는 함수입니다
+function displayMarker(locPosition, message) {
+  // 마커를 생성합니다
+  var marker = new kakao.maps.Marker({
+    map: map,
+    position: locPosition,
+    image: markerImage,
+  });
+
+  var content =
+    '<div class="custom_content">' + "<span>" + message + "</span>" + "</div>";
+
+  // 커스텀 오버레이가 표시될 위치입니다
+  var customOverlay = new kakao.maps.CustomOverlay({
+    map: map,
+    position: locPosition,
+    content: content,
+    yAnchor: 1,
+  });
+  customOverlay.setMap(null);
+
+  kakao.maps.event.addListener(marker, "mouseover", () => {
+    customOverlay.setMap(map);
+  });
+
+  kakao.maps.event.addListener(marker, "mouseout", () => {
+    customOverlay.setMap(null);
+  });
+
+  // 지도 중심좌표를 접속위치로 변경합니다
+  map.setCenter(locPosition);
+}
